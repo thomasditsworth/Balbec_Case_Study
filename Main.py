fc875cb3748a4eefc041a459b0e0c75857d1eeee
Thomas Ditsworth <bugs@gmail.com>
2023-12-17 23:46:16 -0600

    Final Push

--- Main.py
+++ Main.py
@@ -1,24 +1,30 @@
+# Balbec Capital Analyst Case Study
+# For any inquiries, please reach out to thomasjditsworth@gmail.com
+
 import pandas as pd
 
+
+# Function that answers the inital question on the first Excel sheet
 def question1(q1_filepath, excel_file_path):
     column_6th = q1_filepath.iloc[:, 5]
     column_7th = q1_filepath.iloc[:, 6]
-
     right_table_dict = {}
 
+    # Adding all values from the Right table into the dictionary
     for (value_6th, value_7th) in zip(column_6th, column_7th):
         if not pd.isnull(value_6th) and not pd.isnull(value_7th) and (value_6th != 'Property ID'):
             right_table_dict[value_6th] = value_7th
 
-    column_2nd = q1_filepath.iloc[:, 1] # left Property Id
+    column_2nd = q1_filepath.iloc[:, 1]  # left Property Id
     string_to_avoid = "Use a formula to pick out the asset type for the properties on the left from the table on the right."
 
+    # Adding the matching Asset Type to the Right Table
     for index, value_2nd in enumerate(column_2nd):
         if not pd.isnull(value_2nd) and (value_2nd != string_to_avoid):
             temp = right_table_dict.get(value_2nd, "Default")
             q1_filepath.iat[int(index), 2] = temp
 
-    # Use ExcelWriter to append to the original Excel file
+    # Using ExcelWriter to append to the original Excel file
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
         # Remove the existing sheet if it exists
         try:
@@ -26,29 +32,33 @@ def question1(q1_filepath, excel_file_path):
         except KeyError:
             pass
 
-        # Write the DataFrame to the existing or new sheet
+        # Writing the DataFrame to the existing sheet
         q1_filepath.to_excel(writer, sheet_name='Question 1', index=False, header=False)
 
-def question2(q2_filepath, excel_file_path):
 
-    # incorrect way to do it
-    for x in range(6,13):
+# This is an attempt to produce the incorrect methodology originally used on Question 2
+def question2_incorrect(q2_filepath, excel_file_path):
+    # Here, I used nested for loops to simulate iterating over the wrong data in a
+    # way that this type of mistake is believeable
+    for x in range(6, 13):
         val = q2_filepath.iat[x, 1]
         for y in range(x, x + 10):
             temp_val = q2_filepath.iat[y, 4]
             if val == temp_val:
-                q2_filepath.iat[x, 2] = q2_filepath.iat[y,5]
+                q2_filepath.iat[x, 2] = q2_filepath.iat[y, 5]
 
+    # The same method as above to update the table
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
-        # Remove the existing sheet if it exists
         try:
             writer.book.remove(writer.sheets['Question 2'])
         except KeyError:
             pass
 
-        # Write the DataFrame to the existing or new sheet
         q2_filepath.to_excel(writer, sheet_name='Question 2', index=False, header=False)
 
+
+# This is my actual attempt at question 2
+# It uses the same functionality as question 1
 def question2_real(q2_filepath, excel_file_path):
     column_5th = q2_filepath.iloc[:, 4]  # Property ID
     column_6th = q2_filepath.iloc[:, 5]  # Asset Type
@@ -59,7 +69,7 @@ def question2_real(q2_filepath, excel_file_path):
         if not pd.isnull(value_5th) and not pd.isnull(value_6th) and (value_6th != 'ID'):
             right_table_dict[value_5th] = value_6th
 
-    column_2nd = q2_filepath.iloc[:, 1] # left Property Id
+    column_2nd = q2_filepath.iloc[:, 1]  # left Property Id
     string_to_avoid = "What is wrong with the formula in the table to the left? (2 reasons)"
 
     for index, value_2nd in enumerate(column_2nd):
@@ -67,30 +77,34 @@ def question2_real(q2_filepath, excel_file_path):
             temp = right_table_dict.get(value_2nd, "Default")
             q2_filepath.iat[int(index), 2] = temp
 
-    # Use ExcelWriter to append to the original Excel file
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
-        # Remove the existing sheet if it exists
         try:
             writer.book.remove(writer.sheets['Question 2'])
         except KeyError:
             pass
 
-        # Write the DataFrame to the existing or new sheet
         q2_filepath.to_excel(writer, sheet_name='Question 2', index=False, header=False)
 
+
+# My solution to Question 3 is broken up into helper functions to avoid overloading this method
+# Due to the nature of the question, each one of these helper functions operates in the same manner
 def question3(q3_filepath, excel_file_path):
     # Calling helper functions to solve question 3
     question3_helper_type(q3_filepath, excel_file_path)
     question3_helper_currency(q3_filepath, excel_file_path)
     question3_helper_country(q3_filepath, excel_file_path)
 
+
 def question3_helper_country(q3_filepath, excel_file_path):
+    # creating variables to store the sums
     italy = poland = bulgaria = uk = country_total = 0
-    italy_balance = poland_balance = uk_balance = bulgaria_balance= country_total_balance = 0
+    italy_balance = poland_balance = uk_balance = bulgaria_balance = country_total_balance = 0
     percent_sum = percent_sum_balance = 0
-    column_j = q3_filepath.iloc[:, 9] #market
-    column_m = q3_filepath.iloc[:, 12] # country
-    column_k = q3_filepath.iloc[:, 10] # balance
+    # Designating the columns I will be iterating over
+    column_j = q3_filepath.iloc[:, 9]  # market
+    column_m = q3_filepath.iloc[:, 12]  # country
+    column_k = q3_filepath.iloc[:, 10]  # balance
+    # Calculating the sums
     for (val_j, val_m, val_k) in zip(column_j, column_m, column_k):
         if not isinstance(val_j, int):
             continue
@@ -108,6 +122,8 @@ def question3_helper_country(q3_filepath, excel_file_path):
             bulgaria_balance += val_k
         country_total += val_j
         country_total_balance += val_k
+
+    # Updating the sums on the Excel Sheet
     q3_filepath.iat[19, 2] = italy
     percent_sum += italy / country_total
     q3_filepath.iat[19, 3] = italy / country_total
@@ -147,23 +163,23 @@ def question3_helper_country(q3_filepath, excel_file_path):
     q3_filepath.iat[23, 5] = percent_sum_balance
 
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
-        # Remove the existing sheet if it exists
         try:
             writer.book.remove(writer.sheets['Question 3'])
         except KeyError:
             pass
 
-        # Write the DataFrame to the existing or new sheet
         q3_filepath.to_excel(writer, sheet_name='Question 3', index=False, header=False)
 
 
 def question3_helper_currency(q3_filepath, excel_file_path):
+    # Creating variables to store the sums
     EUR = GBP = BGN = PLN = currency_total = 0
     EUR_balance = GBP_balance = BGN_balance = PLN_balance = currency_total_balance = 0
     percent_sum = percent_sum_balance = 0
-    column_j = q3_filepath.iloc[:, 9] #market
-    column_l = q3_filepath.iloc[:, 11] # currency
-    column_k = q3_filepath.iloc[:, 10] # balance
+    # Designating the columns
+    column_j = q3_filepath.iloc[:, 9]  # market
+    column_l = q3_filepath.iloc[:, 11]  # currency
+    column_k = q3_filepath.iloc[:, 10]  # balance
     for (val_j, val_l, val_k) in zip(column_j, column_l, column_k):
         if not isinstance(val_j, int):
             continue
@@ -181,9 +197,8 @@ def question3_helper_currency(q3_filepath, excel_file_path):
             PLN_balance += val_k
         currency_total += val_j
         currency_total_balance += val_k
-    print(currency_total_balance)
-    print(currency_total)
 
+    # Editing the Excel Sheet
     q3_filepath.iat[12, 2] = EUR
     percent_sum += EUR / currency_total
     q3_filepath.iat[12, 3] = EUR / currency_total
@@ -200,9 +215,8 @@ def question3_helper_currency(q3_filepath, excel_file_path):
     percent_sum += PLN / currency_total
     q3_filepath.iat[15, 3] = PLN / currency_total
 
-    q3_filepath.iat[16,2] = currency_total
-    q3_filepath.iat[16,3] = percent_sum
-
+    q3_filepath.iat[16, 2] = currency_total
+    q3_filepath.iat[16, 3] = percent_sum
 
     q3_filepath.iat[12, 4] = EUR_balance
     percent_sum_balance += EUR_balance / currency_total_balance
@@ -220,26 +234,32 @@ def question3_helper_currency(q3_filepath, excel_file_path):
     percent_sum_balance += PLN_balance / currency_total_balance
     q3_filepath.iat[15, 5] = PLN_balance / currency_total_balance
 
-    q3_filepath.iat[16,4] = currency_total_balance
-    q3_filepath.iat[16,5] = percent_sum_balance
+    q3_filepath.iat[16, 4] = currency_total_balance
+    q3_filepath.iat[16, 5] = percent_sum_balance
 
+    # Uploading to the Excel Sheet
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
-        # Remove the existing sheet if it exists
         try:
             writer.book.remove(writer.sheets['Question 3'])
         except KeyError:
             pass
 
-        # Write the DataFrame to the existing or new sheet
         q3_filepath.to_excel(writer, sheet_name='Question 3', index=False, header=False)
 
+
 def question3_helper_type(q3_filepath, excel_file_path):
+    # Creating variables to store the sum
+
     residential = commercial = land = industrial = other = type_total = 0
     residential_balance = commercial_balance = land_balance = industrial_balance = other_balance = type_total_balance = 0
     percent_sum = percent_sum_balance = 0
+
+    # Designating the columns to iterate over
     column_j = q3_filepath.iloc[:, 9]
     column_i = q3_filepath.iloc[:, 8]
     column_k = q3_filepath.iloc[:, 10]
+
+    # Adding the values to the summation variables
     for (val_j, val_i, val_k) in zip(column_j, column_i, column_k):
         if not isinstance(val_j, int) or val_j is None:
             continue
@@ -260,53 +280,55 @@ def question3_helper_type(q3_filepath, excel_file_path):
             other_balance += val_k
         type_total += val_j
         type_total_balance += val_k
-    print(type_total)
-    print(type_total_balance)
-    q3_filepath.iat[4,2] = residential
+
+    # Editing the Excel Sheet
+    q3_filepath.iat[4, 2] = residential
     percent_sum += residential / type_total
-    q3_filepath.iat[4,3] = residential / type_total
+    q3_filepath.iat[4, 3] = residential / type_total
 
-    q3_filepath.iat[5,2] = commercial
+    q3_filepath.iat[5, 2] = commercial
     percent_sum += commercial / type_total
-    q3_filepath.iat[5,3] = commercial / type_total
+    q3_filepath.iat[5, 3] = commercial / type_total
 
-    q3_filepath.iat[6,2] = industrial
+    q3_filepath.iat[6, 2] = industrial
     percent_sum += industrial / type_total
-    q3_filepath.iat[6,3] = industrial / type_total
+    q3_filepath.iat[6, 3] = industrial / type_total
 
-    q3_filepath.iat[7,2] = land
+    q3_filepath.iat[7, 2] = land
     percent_sum += land / type_total
-    q3_filepath.iat[7,3] = land / type_total
+    q3_filepath.iat[7, 3] = land / type_total
 
-    q3_filepath.iat[8,2] = other
+    q3_filepath.iat[8, 2] = other
     percent_sum += other / type_total
-    q3_filepath.iat[8,3] = other / type_total
+    q3_filepath.iat[8, 3] = other / type_total
 
-    q3_filepath.iat[9,2] = type_total
-    q3_filepath.iat[9,3] = percent_sum
+    q3_filepath.iat[9, 2] = type_total
+    q3_filepath.iat[9, 3] = percent_sum
 
-    q3_filepath.iat[4,4] = residential_balance
+    q3_filepath.iat[4, 4] = residential_balance
     percent_sum_balance += residential_balance / type_total_balance
-    q3_filepath.iat[4,5] = residential_balance / type_total_balance
+    q3_filepath.iat[4, 5] = residential_balance / type_total_balance
 
-    q3_filepath.iat[5,4] = commercial_balance
+    q3_filepath.iat[5, 4] = commercial_balance
     percent_sum_balance += commercial_balance / type_total_balance
-    q3_filepath.iat[5,5] = commercial_balance / type_total_balance
+    q3_filepath.iat[5, 5] = commercial_balance / type_total_balance
 
-    q3_filepath.iat[6,4] = industrial_balance
+    q3_filepath.iat[6, 4] = industrial_balance
     percent_sum_balance += industrial_balance / type_total_balance
-    q3_filepath.iat[6,5] = industrial_balance / type_total_balance
+    q3_filepath.iat[6, 5] = industrial_balance / type_total_balance
 
-    q3_filepath.iat[7,4] = land_balance
+    q3_filepath.iat[7, 4] = land_balance
     percent_sum_balance += land_balance / type_total_balance
-    q3_filepath.iat[7,5] = land_balance / type_total_balance
+    q3_filepath.iat[7, 5] = land_balance / type_total_balance
 
-    q3_filepath.iat[8,4] = other_balance
+    q3_filepath.iat[8, 4] = other_balance
     percent_sum_balance += other_balance / type_total_balance
-    q3_filepath.iat[8,5] = other_balance / type_total_balance
+    q3_filepath.iat[8, 5] = other_balance / type_total_balance
 
-    q3_filepath.iat[9,4] = type_total_balance
-    q3_filepath.iat[9,5] = percent_sum_balance
+    q3_filepath.iat[9, 4] = type_total_balance
+    q3_filepath.iat[9, 5] = percent_sum_balance
+
+    # Uploading to the Excel File
     with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
         # Remove the existing sheet if it exists
         try:
@@ -318,17 +340,19 @@ def question3_helper_type(q3_filepath, excel_file_path):
         q3_filepath.to_excel(writer, sheet_name='Question 3', index=False, header=False)
 
 
-
 def main():
-    # Read Excel file
+    # Read In The Excel file
     excel_file_path = r'C:\Users\thoma\PycharmProjects\Balbec_Case_Study\Analyst Case Study Balbec Capital Python.xlsx'
     sheet1 = pd.read_excel(excel_file_path)
     sheet2 = pd.read_excel(excel_file_path, sheet_name=1)
     sheet3 = pd.read_excel(excel_file_path, sheet_name=2)
 
-
+    # Calling each of the functions
     question1(sheet1, excel_file_path)
     question2_real(sheet2, excel_file_path)
     question3(sheet3, excel_file_path)
+
+    print("The code has finished! Thank you for your time - Tom Ditsworth")
+
 if __name__ == "__main__":
     main()
